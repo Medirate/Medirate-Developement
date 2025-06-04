7dc07e1e1b8b79357564fb8b02506ab4ee9496c7
Medirate <medirate.net@gmail.com>
2025-06-04 17:45:17 +0530

    27th May risk

--- src/app/state-rate-comparison/page.tsx
+++ src/app/state-rate-comparison/page.tsx
@@ -1641,7 +1641,7 @@ export default function StatePaymentComparison() {
                                     onClick={() => handleTableRowSelection(state, item)}
                                             className="text-gray-400 hover:text-red-500 transition-colors"
                                             title="Deselect"
-                                          >
+                                  >
                                             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                                               <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
                                             </svg>
